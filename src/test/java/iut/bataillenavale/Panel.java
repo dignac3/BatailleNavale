05daceff25f092df78279d81d81b8c775b39539e
doria <doria@192.168.0.19>
2017-05-03 21:46:59 +0200

    uo

---
+++ src/test/java/iut/bataillenavale/Panel.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package iut.bataillenavale;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/**
+ * @author doria
+ *
+ */
+public class Panel extends JPanel {
+	
+	private JButton jb_demarrer = new JButton("Démarrer");
+	
+	
+	public Panel(){
+		
+		
+	}
+}
